8bcab91b5cfc72ac3f839192b4ccae881a371a3c
Liam Palmqvist <Liam.palmqvist@icloud.com>
2023-02-09 14:31:06 +0000

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+// 3.
+console.log("Hello, World!");
+
+// Initialising variables
+// 4.a
+let a = 5;
+//4.b
+console.log("Variable a is " + a);
+
+// Variables can be multiplied by eachother
+//4.c
+let b = 3;
+//4.d
+console.log("Variable a + variable b is " + (a*b));
+//4.e
+console.log("Variable a + variable b modulo 5 is " + (a*b)%5);
+
+//4.f
+s1 = "Hello";
+s2 = "Liam";
+
+//4.g
+console.log(s1+s2);
+
+//4.h
+console.log(s1 + " " + s2);
+
+//4.i
+console.log("5 squared is " + a*a)
+
